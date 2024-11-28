7cd2db40eb9368b0b4d5b6b355c782438d3aba9a
Will Harmer <94372378+William-Harmer@users.noreply.github.com>
2024-11-28 10:50:33 +0000

    Fixed unit propagation bug where if there was a negation of a literal in a single clause, that clause wouldn't be removed from the arraylist and so there was just an empty clause.

--- src/UnitPropagation.java
+++ src/UnitPropagation.java
@@ -8,29 +8,45 @@ public class UnitPropagation {
 
         do {
             formulaModified = false; // Reset flag at the start of each iteration
+            System.out.println("Starting a new iteration");
 
             for (int j = 0; j < clauses.size(); j++) { // For each clause
                 ArrayList<Character> clause = clauses.get(j); // Get the clause
+                System.out.println("Checking clause " + j + ": " + clause);
 
                 if (clause.size() == 1) { // Check if it is a unit clause
                     char unitClauseElement = clause.getFirst(); // Get the unit clause element
+                    System.out.println("Found unit clause with element: " + unitClauseElement);
 
                     for (int i = 0; i < clauses.size(); i++) { // For each clause
                         if (i == j) continue; // Apart from its own
                         ArrayList<Character> currentClause = clauses.get(i);
+                        System.out.println("Checking clause " + i + ": " + currentClause);
 
                         if (currentClause.contains(unitClauseElement)) { // If clause contains the unit clause element
+                            System.out.println("Clause " + i + " contains the unit element " + unitClauseElement);
                             clauses.remove(i); // Remove the whole clause
                             i--; // Adjust index
                             j--; // Adjust j due to removal
                             formulaModified = true; // Record modification
+                            System.out.println("Removed clause " + i + ", new clauses: " + clauses);
                         } else if (currentClause.contains(Utility.negate(unitClauseElement))) { // If negated version is found
-                            clauses.get(i).remove((Character) Utility.negate(unitClauseElement)); // Remove the element
+                            if(currentClause.size() == 1) { // If the negated literal is just by itself in a clause, remove the whole clause
+                                clauses.remove(i);
+                                i--;
+                                j--;
+                            } else { // Otherwise just remove that element
+                                clauses.get(i).remove((Character) Utility.negate(unitClauseElement));
+                            }
                             formulaModified = true; // Record modification
+
+                            System.out.println("Clause " + i + " contains the negation of " + unitClauseElement);
+                            System.out.println("Updated clause " + i + ": " + currentClause);
                         }
                     }
 
                     if (formulaModified) {
+                        System.out.println("Formula modified, restarting processing unit clauses");
                         break; // Restart processing all unit clauses
                     }
                 }
